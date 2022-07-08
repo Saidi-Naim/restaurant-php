61a1d585697c233d45e73077f3cd62c0f71515b7
saidi naim <104205932+Saidi-Naim@users.noreply.github.com>
2022-07-08 23:39:19 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     // $con = new mysqli('localhost','root','Naim-Camil@123','restaurant');
-    $con= new PDO('mysql:host=localhost;dbname=restaurant;charset=utf8','root','Naim-Camil@123');
+    $con= new PDO('mysql:host=localhost;dbname=restaurant;charset=utf8','root','');
 
     if(!$con){
         die(mysqli_error($con));
